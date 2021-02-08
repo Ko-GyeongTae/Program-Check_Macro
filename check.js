7ac7230e29c3bd743c0c2fdc739faf6e7cbe64d1
Ko-GyeongTae <kokt0203@naver.com>
2021-02-08 12:06:15 +0900

    Edit: check.js

---
+++ check.js
@@ -0,0 +1,30 @@
+const puppeteer = require('puppeteer');
+
+async function begin() {
+    console.log("Check Process Start...");
+    const browser = await puppeteer.launch({headless:false, defaultViewport:null});
+    const page = await browser.newPage();
+    console.log("Created browser & page");
+    try{
+        await page.goto('https://chang14.netlify.app/login');
+        await page.type('#root > form > input[type=text]:nth-child(1)', 'iovsi');
+        await page.type('#root > form > input[type=text]:nth-child(2)', '고경태');
+        console.log('Login..');
+        
+        await page.click('#root > form > button');
+        await page.waitForNavigation({ waitUntil: 'networkidle0'});
+     
+        console.log('Login Success!');
+        let i;
+        for(i = 1; i <= 5; i++){
+            await page.click('#root > div > div.sc-dlfnbm.bcaJjD > div.sc-jSgupP.ckDfJz');
+            console.log('Morning Check!');
+
+            await Handle_alert(page);
+            console.log(`Reloading... : ${i}`);
+        }
+        
+    } catch(error) {
+        console.log(error);
+    }
+}
